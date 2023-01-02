dee150519dec013ff943ee3e5101365cdeae3810
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-01-02 16:10:56 -0700

    files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Website Header with Bootstrap Carousel
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jotadagostin/pen/YzjXGbQ](https://codepen.io/jotadagostin/pen/YzjXGbQ).
+
+This is a pen where you can know how to create a website header with Bootstrap Carousel. So, the header will have images changing with an interval between them
---
+++ dist/script.js
@@ -0,0 +1,29 @@
+/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
+$('.carousel').carousel({
+    pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
