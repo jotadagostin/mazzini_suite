e059b00318314be15e06f7539e88b6dc4388fbe7
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-02-28 19:10:58 +0100

    html and css

--- script.js
+++ script.js
@@ -27,3 +27,38 @@ window.addEventListener("scroll", function () {
         document.querySelector("#navbar").style.opacity = 1;
     }
 });
+
+// Cards review js code:
+
+$.fn.commentCards = function(){
+
+    return this.each(function(){
+  
+      var $this = $(this),
+          $cards = $this.find('.card'),
+          $current = $cards.filter('.card--current'),
+          $next;
+  
+      $cards.on('click',function(){
+        if ( !$current.is(this) ) {
+          $cards.removeClass('card--current card--out card--next');
+          $current.addClass('card--out');
+          $current = $(this).addClass('card--current');
+          $next = $current.next();
+          $next = $next.length ? $next : $cards.first();
+          $next.addClass('card--next');
+        }
+      });
+  
+      if ( !$current.length ) {
+        $current = $cards.last();
+        $cards.first().trigger('click');
+      }
+  
+      $this.addClass('cards--active');
+  
+    })
+  
+  };
+  
+  $('.cards').commentCards();
