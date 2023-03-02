4abe5dc0a5d66e0dea46537b1a1c8c3fdfad11a6
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-02 15:34:28 +0100

    footer style

--- script.js
+++ script.js
@@ -1,64 +1,61 @@
-/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
-$('.carousel').carousel({
-    pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
+/* When your mouse cursor enter the background, the fading won't pause and keep playing */
+$(".carousel").carousel({
+  pause:
+    "false" /* Change to true to make it paused when your mouse cursor enter the background */,
 });
 
 // Smooth Scrolling
 $("#navbar a, .btn").on("click", function (event) {
-    if (this.hash !== "") {
-        event.preventDefault();
+  if (this.hash !== "") {
+    event.preventDefault();
 
-        const hash = this.hash;
+    const hash = this.hash;
 
-        $("html, body").animate(
-            {
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
 });
 
 // Sticky menu background
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 150) {
-        document.querySelector("#navbar").style.opacity = 0.9;
-    } else {
-        document.querySelector("#navbar").style.opacity = 1;
-    }
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
 });
 
 // Cards review js code:
 
-$.fn.commentCards = function(){
+$.fn.commentCards = function () {
+  return this.each(function () {
+    var $this = $(this),
+      $cards = $this.find(".card"),
+      $current = $cards.filter(".card--current"),
+      $next;
 
-    return this.each(function(){
-  
-      var $this = $(this),
-          $cards = $this.find('.card'),
-          $current = $cards.filter('.card--current'),
-          $next;
-  
-      $cards.on('click',function(){
-        if ( !$current.is(this) ) {
-          $cards.removeClass('card--current card--out card--next');
-          $current.addClass('card--out');
-          $current = $(this).addClass('card--current');
-          $next = $current.next();
-          $next = $next.length ? $next : $cards.first();
-          $next.addClass('card--next');
-        }
-      });
-  
-      if ( !$current.length ) {
-        $current = $cards.last();
-        $cards.first().trigger('click');
+    $cards.on("click", function () {
+      if (!$current.is(this)) {
+        $cards.removeClass("card--current card--out card--next");
+        $current.addClass("card--out");
+        $current = $(this).addClass("card--current");
+        $next = $current.next();
+        $next = $next.length ? $next : $cards.first();
+        $next.addClass("card--next");
       }
-  
-      $this.addClass('cards--active');
-  
-    })
-  
-  };
-  
-  $('.cards').commentCards();
+    });
+
+    if (!$current.length) {
+      $current = $cards.last();
+      $cards.first().trigger("click");
+    }
+
+    $this.addClass("cards--active");
+  });
+};
+
+$(".cards").commentCards();
