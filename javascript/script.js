847a4f66472331863252c2c4b8ece9fe96dc48ac
João Paulo Dagostin <dagostinjota@gmail.com>
2025-05-04 13:04:33 +0200

    fixing target blank error

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,10 @@ document.querySelectorAll("#menu a").forEach((link) => {
 
 document.documentElement.style.overflowX = "hidden";
 document.body.style.overflowX = "hidden";
+
+// fixing a target blank problem:
+
+window.open(
+  "https://www.freetour.com/verona?utm_source=google&utm_medium=cpc&utm_campaign=Pmax_Italy_Top_AB&utm_term=&gad_source=1&gad_campaignid=17305331886&gclid=Cj0KCQjw8cHABhC-ARIsAJnY12xO464b5T-Jdn00YWNYBo3i7hRYSYxaQsdNgKieO4y0xJ3YlrqnMeAaAvs6EALw_wcB",
+  "_blank"
+);
